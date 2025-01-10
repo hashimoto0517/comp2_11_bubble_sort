594fa4c7584af0bb4fb8dace0a28754ff917500b
hashimoto0517 <izumi.gsmz1@gmail.com>
2025-01-11 00:44:08 +0900

    実装しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,25 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (begin == nullptr || end == nullptr || begin >= end) 
+    {
+        return false;
+    }
+
+    size_t length = end - begin;
+
+    for (size_t i = 0; i < length - 1; i++) 
+    {
+        for (size_t j = 0; j < length - 1 - i; j++)
+        {
+            if (begin[j].key > begin[j + 1].key) 
+            {
+                item tmp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = tmp;
+            }
+        }
+    }
+
+    return true;
 }
